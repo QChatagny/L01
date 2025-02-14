7e362097a10d4ca9a10816bc4c9d7afdf20c0248
Quentin Chatagny <e.qchatagny2@etu.cvm.qc.ca>
2025-02-14 08:51:01 -0500

    removed test cpp and header

--- L01-Quentin-Chatagny/tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "C:\Users\quent\source\Repos\L01-Quentin-Chatagny\L01-Quentin-Chatagny\tests.h"
-#include "C:\Users\quent\source\repos\L01-Quentin-Chatagny\L01-Quentin-Chatagny\main.cpp"
-
-
-void test_Ak_input_move_to(Move m, Ak input) {
-
-}
